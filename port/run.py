368a98cdce927a62a20afae24d49743f4a8c2867
Coo1kie <alegm3865@gmail.com>
2021-04-14 00:28:05 -0400

    despedida

--- port/run.py
+++ port/run.py
@@ -3,6 +3,9 @@ app=Flask(__name__)
 @app.route("/")
 def index():
     return("HELLO WORLD")
+@app.route("/despedida")
+def despedida():
+    return("bye WORLD XD")
 if __name__ == "__main__":
     pass
     app.run(debug=True,port=5001)
