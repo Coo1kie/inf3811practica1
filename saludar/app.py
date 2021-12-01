8a70e4e41ec5fe70203759bc44d1ec088f3ddecf
Alejandra Gutierrez M <49824697+Coo1kie@users.noreply.github.com>
2021-12-01 02:58:41 -0400

    Update app.py

--- saludar/app.py
+++ saludar/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def index():
-    return("HOLA MUNDO")
+    return("HOLA MUNDO XD")
 if __name__ == "__main__":
     pass
     app.run()
